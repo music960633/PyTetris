b16919e6cf73b43461bcfcb26b0bfc8938293901
music960633 <music960633@gmail.com>
2016-01-10 21:40:04 +0800

    add menu

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from pygame.locals import *
 from game import *
+from menu import *
 from defines import *
 
 def main():
@@ -9,36 +10,11 @@ def main():
   pygame.display.set_caption("PyTetris")
   screen = pygame.display.set_mode(SCREEN_SIZE)
   
-  menu(screen)
-
-def menu(screen):
-  while True:
-    f = pygame.font.SysFont("Arial", 20)
-    text1 = f.render("F1: 1P mode with auto attack", 1, WHITE)
-    text2 = f.render("F2: 1P mode with invisible blocks", 1, WHITE)
-    text3 = f.render("F3: 4-wide practice", 1, WHITE)
-    centerx, centery = screen.get_rect().center
-    screen.fill(BLACK)
-    screen.blit(text1, (centerx - 200, centery - 30))
-    screen.blit(text2, (centerx - 200, centery     ))
-    screen.blit(text3, (centerx - 200, centery + 30))
-    pygame.display.flip()
-    game = None
-    
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT: sys.exit()
-      if event.type == pygame.KEYDOWN:
-        if event.key == K_F1:
-          game = Game1P(screen)
-        elif event.key == K_F2:
-          game = Game1PInvisible(screen)
-        elif event.key == K_F3:
-          game = Game1P4W(screen)
-
-    if game is not None:
-      game.start()
-
-    pygame.time.Clock().tick(120)
+  menu = Menu(isRoot = True)
+  menu.addOption("1P mode with auto attack"     , Game1P(screen).start)
+  menu.addOption("1P mode with invisible blocks", Game1PInvisible(screen).start)
+  menu.addOption("4-wide practice"              , Game1P4W(screen).start)
+  menu.start()
 
 if __name__ == "__main__":
   main()
---
+++ menu.py
@@ -0,0 +1,56 @@
+import pygame
+import sys
+from pygame.locals import *
+from defines import *
+
+class Menu:
+  def __init__(self, isRoot):
+    self.options = []
+    self.actions = []
+    self.pointer = 0
+    self.isRoot = isRoot
+    self.clock = pygame.time.Clock()
+
+  def start(self):
+    assert len(self.options) > 0, "menu is empty!"
+    while True:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.KEYDOWN:
+          # next option
+          if event.key == K_DOWN: 
+            self.pointer += 1
+            self.pointer %= len(self.options)
+          # previous option
+          elif event.key == K_UP: 
+            self.pointer -= 1
+            self.pointer %= len(self.options)
+          # select option
+          elif event.key == K_RETURN:
+            self.actions[self.pointer]()
+          # return to previous menu
+          elif event.key == K_LEFT: 
+            if not self.isRoot: return
+      
+      self.draw()
+      pygame.display.flip()
+      self.clock.tick(120)
+
+  # add an option and corresponding action
+  # if the option is selected, do the action
+  def addOption(self, option, action):
+    self.options.append(option)
+    self.actions.append(action)
+
+  def draw(self):
+    surface = pygame.display.get_surface()
+    f = pygame.font.SysFont("Consolas", 24)
+    sum_height = 0
+    # fill black background
+    surface.fill(BLACK)
+    # blit options
+    for i in range(len(self.options)):
+      color = YELLOW if i == self.pointer else WHITE
+      text = f.render(self.options[i], 1, color)
+      surface.blit(text, (0, sum_height))
+      sum_height += text.get_height()
