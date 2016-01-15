ab26d978b63e54ae6dc3faf93b10ed1859153536
music960633 <music960633@gmail.com>
2016-01-15 21:17:14 +0800

    add attack calculation to GameInfo

--- info.py
+++ info.py
@@ -6,23 +6,27 @@ class GameInfo:
     self.combo   = 0
     self.b2b     = True
 
-  def getInfo(self):
-    return self.lineTot, self.atkTot, self.combo, self.b2b
-
-  def getLine(self):
-    return self.lineTot
-
-  def getAtk(self):
-    return self.atkTot
-
-  def getCombo(self):
-    return self.combo
-
-  def getB2b(self):
-    return self.b2b
-
-  def update(self, line, atk, tspin):
+  def updateAndGetAtk(self, line, tspin):
+    atk = self.calcAtk(line, tspin)
     self.lineTot += line
-    self.atkTot  += atk
-    self.combo   = (self.combo + 1) if line > 0 else 0
-    self.b2b     = True if (line == 4 or tspin) else False
+    self.atkTot += self.calcAttack(line, tspin)
+    self.combo = (self.combo + 1) if line > 0 else 0
+    self.b2b = self.b2b if line == 0 else (line == 4 or tspin)
+    return atk
+
+  def calcAtk(self, line, tspin):
+    # normal attack
+    if not tspin:
+      assert 0 <= line <= 4, "invalid lines"
+      if not self.b2b:
+        return (0, 0, 1, 2, 4)[line]
+      else:
+        return (0, 0, 1, 2, 5)[line]
+    # T spin attack
+    else:
+      assert 0 <= line <= 3, "invalid lines"
+      if not self.b2b:
+        return (0, 2, 4, 6)[line]
+      else:
+        return (0, 3, 5, 8)[line]
+
